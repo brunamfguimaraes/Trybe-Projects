4ebe56bdf399c084084b718f18369da974547525
Bruna Ferreira <brunamfguimaraes@gmail.com>
2020-11-25 18:20:53 -0300

    Iniciando repositório e incluindo a primeira atividade

---
+++ exercises/bloco_4/dia_1/exercise1.js
@@ -0,0 +1,22 @@
+let a = 2;
+let b = 5;
+console.log (a + b);
+
+let a = 3;
+let b = 2;
+console.log(a - b);
+
+let a = 10;
+let b = 2;
+console.log(a / b);
+
+let a = 3;
+let b = 4;
+console.log(a * b);
+
+let a = 4;
+let b = 2;
+console.log(a % b);
+
+
+
