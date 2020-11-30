8aba17c50816077bf28622e8522a276e6b8a1d90
Bruna Ferreira <brunamfguimaraes@gmail.com>
2020-11-30 11:43:13 -0300

    Finalizando atividades do dia 2

--- exercises/bloco_4/dia_2/exercises1.js
+++ exercises/bloco_4/dia_2/exercises1.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //let numbers = [2, 4, 6, 8];
 /*
 exercicio 1
@@ -13,9 +13,10 @@ for (let index = 0; index < numbers.length; index++) {
 }
     console.log(total);
     total
-let sum = 0;*/
+*/
 
 /* Exercicio 3
+let sum = 0;
 for (let i = 0; i < numbers.length; i ++) {
     sum += numbers[i];
 }
@@ -51,20 +52,39 @@ for (let i = 0; i < numbers.length; i++) {
         } else {
             console.log(resultado);
         } */
+
     
+ /*Exercicio 7
+    let menor = 1000;
 
+    for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < menor) {
+        menor = numbers[i];
+    } 
+}
+    console.log(menor); */
 
+/*Exercicio 8 e 9
+let numeros = [];
+    for (let i = 1; i <= 25; i += 1) {
+    numeros.push(i);
+    }
+    console.log(numeros);
 
+    for (let i = 0; i < numeros.length; i += 1) {
+        console.log(numeros[i] / 2);
+    };*/
 
 
 
 /*1 Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 2 Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
-3Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+3Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;let soma = 0;
+
 A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
 4 Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 5 Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 6 Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 7 Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 8 Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
-9 Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .*/
+//9 Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2. */
