efb97cf9432dcb0a0d89ae71320974e099987198
Bruna Ferreira <brunamfguimaraes@gmail.com>
2020-11-30 11:44:37 -0300

    Atualizando atividade

---
+++ exercises/bloco_4/dia_3/exercises1.js
@@ -0,0 +1,28 @@
+let n = '';
+for (let i = 0; i <= 4; i +=1) {
+     //console.log(n);
+
+      // if (i <= 4) {
+        n = n + "*";
+    //}
+    console.log(n);
+
+}       
+// console.log(n);
+
+    /*let n = '*****';
+    for (let i = 0; i < 5; i +=1) {
+        console.log(n);
+
+        //if (i * 5) {
+          //  n = n + "*";
+        //}
+    }
+    /*console.log(n);
+    console.log(n);
+    console.log(n);
+    console.log(n);
+    console.log(n);*/
+
+
+
