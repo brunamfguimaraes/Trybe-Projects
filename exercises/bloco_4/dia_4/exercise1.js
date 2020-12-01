95ace037731c5582731a0de0fc84ff1baa2bd281
Bruna Ferreira <brunamfguimaraes@gmail.com>
2020-12-01 15:03:07 -0300

    Corrigindo exercicios

---
+++ exercises/bloco_4/dia_4/exercise1.js
@@ -0,0 +1,29 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "sim",
+  };
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christimas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "MacPatinhas",
+    recorrente: "Sim",
+}
+
+    for (let property in info) {
+        console.log(info[property], info2[property]);
+    }
+
+
+
+
+
+
+    //console.log('Bem vinda, ' + info.personagem);
+
+    /*for (let key in info) {
+        console.log(info[key]);
+    };*/
+
